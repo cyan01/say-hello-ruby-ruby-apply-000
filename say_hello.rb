dcc88485cb5dce2e3b31ef1bb36d2fc8f69390ff
Cyan Williams <cyan.williams16@myhunter.cuny.edu>
2020-05-01 22:34:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello(name, language="Ruby")
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+def say_hello(name)
+  puts "Hello, {name}!"
 end
